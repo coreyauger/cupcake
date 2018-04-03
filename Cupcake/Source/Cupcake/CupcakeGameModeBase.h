5171d2b582b3913471a8975d68742a1afd62773b
coreyauger <coreyauger@gmail.com>
2018-04-03 12:01:01 -0700

    initial commit

---
+++ Cupcake/Source/Cupcake/CupcakeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CupcakeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUPCAKE_API ACupcakeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
